ddf027977df7e002960c8b0ee4803c5d2b409985
Andrei Tarasevich <tarasevich_a@ddc.by>
2024-03-13 12:26:38 +0300

    init: start project

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Page() {
@@ -16,6 +17,9 @@ export default function Page() {
               Next.js Learn Course
             </a>
             , brought to you by Vercel.
+            <div
+              className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
+            />
           </p>
           <Link
             href="/login"
@@ -25,7 +29,12 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            alt="Some information"
+            width={1000}
+            height={760}
+          />
         </div>
       </div>
     </main>
